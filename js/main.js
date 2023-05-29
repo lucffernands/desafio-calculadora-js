69d3fd7814e45eeec2d38b14a2c2d6b70da69446
lucffernands <lucianoffernands@gmail.com>
2023-05-29 17:33:06 -0300

    hundle inputError

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
-const n1 = document.querySelector('#number1');
-const n2 = document.querySelector('#number2');
+const inputNumber1 = document.querySelector('#number1');
+const inputNumber2 = document.querySelector('#number2');
 const result = document.querySelector('#result-box');   
 
 function sum() {
-    result.innerHTML = parseInt(n1.value) + parseInt(n2.value);
+    const n1 = inputNumber1.value;
+    const n2 = inputNumber2.value;
+
+    if(isNumber(n1) && isNumber(n2)) {
+        result.innerHTML = (+n1) + (+n2);
+
+    }else if(!isNumber(n1) && !isNumber(n2)){
+        inputNumber1.classList.add('input-error');
+        inputNumber2.classList.add('input-error');
+
+    }else if(!isNumber(n1)){
+        inputNumber1.classList.add('input-error');
+
+    }else if(!isNumber(n2)){
+        inputNumber2.classList.add('input-error');
+    }
+  
 }
 
 function multiply() {
-    result.innerHTML = parseInt(n1.value) * parseInt(n2.value);
+    const n1 = inputNumber1.value;
+    const n2 = inputNumber2.value;
+    result.innerHTML = (+n1) * (+n2);
 }
 
 function clean() {
-    n1.value = '';
-    n2.value = '';
+    inputNumber1.value = '';
+    inputNumber2.value = '';
     result.innerHTML = 0;
-    document.querySelector(".input-error").classList.remove('input-error');
+    inputNumber1.classList.remove('input-error');
+    inputNumber2.classList.remove('input-error');
 }
 
-
+function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
